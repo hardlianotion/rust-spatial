7d1fa34df909fe5cf116de43a64e5170c10d90ac
Etuka Onono <eonono@persistentnotions.co.uk>
2024-05-07 19:01:51 +0200

    Set up precommit hook and CI.

--- src/tree.rs
+++ src/tree.rs
@@ -1,16 +1,31 @@
-pub enum TreeNode <T> {
-    Leaf (T),
-    Node ([Box<TreeNode<T>>;6])
+pub enum TreeNode<T> {
+    Leaf(T),
+    Node([Box<TreeNode<T>>; 6]),
 }
 
-impl <T> TreeNode <T> {
-  pub fn get (&self) -> &TreeNode <T> {
-    todo! ("Get the node at this address");
-  }
-
-  pub fn contains (&self) -> bool {
-    todo!("Check that tree contains index.");
-  }
-
+#[derive(Clone, Copy, Debug)]
+pub struct Index {
+    pub index: u64,
 }
 
+pub struct H3Tree<T> {
+    pub root: TreeNode<T>,
+}
+
+impl<T> H3Tree<T> {
+    pub fn get(&self, _index: Index) -> Option<&T> {
+        todo!("Get the node at this address");
+    }
+
+    pub fn contains(&self, _index: Index) -> bool {
+        todo!("Check that tree contains index.");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_fail() {
+        assert!(false);
+    }
+}
